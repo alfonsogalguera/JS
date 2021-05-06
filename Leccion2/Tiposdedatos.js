2bfda53c602e891eb32a6d20e8319e815a9d31db
alfonsogalguera <57268703+alfonsogalguera@users.noreply.github.com>
2021-05-06 18:11:04 -0500

    js
    js

---
+++ Leccion2/Tiposdedatos.js
@@ -0,0 +1,18 @@
+//Tipo de dato string
+var nombre = "Alfonso";
+console.log(nombre);
+
+nombre=10;
+console.log(nombre);
+//Tipo de dato numerico
+var numero=1000;
+console.log(numero);
+
+//Tipo de dato objeto
+
+var objeto={
+    nombre : "Juan", 
+    apellido : "perez",
+    telefono : "9581008481"
+}
+console.log(objeto);
