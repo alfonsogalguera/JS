32c7a504e84eafac1d8b528964c3161f01aa7091
alfonsogalguera <57268703+alfonsogalguera@users.noreply.github.com>
2021-06-08 13:46:56 -0500

    leccion7
    kjnfijsd

--- Leccion6/arreglos.js
+++ Leccion6/arreglos.js
@@ -1,4 +1,26 @@
 //let alitas = new Array();
 
 const precio = ['80', '100', '150', '200','300'];
+for(let i = 0; i<precio.length; i++){
+    console.log(i+": "+precio[i]);
+}
+precio[2]=120;
 console.log(precio);
+
+precio.push('110');
+console.log(precio);
+
+precio[precio.length]='222';
+console.log(precio);
+
+precio[7]='666';
+console.log(precio);
+
+//Preguntar si es un array
+
+console.log(typeof precio);
+
+Array.isArray(precio);
+console.log(Array.isArray(precio));
+
+console.log(precio instanceof Array);
