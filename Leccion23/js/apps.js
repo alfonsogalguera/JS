950717ba3c24b77bfa43dcdc8d1fd9fe486f2dc5
alfonsogalguera <57268703+alfonsogalguera@users.noreply.github.com>
2021-08-02 13:01:38 -0500

    js
    js

--- Leccion23/js/apps.js
+++ Leccion23/js/apps.js
@@ -12,3 +12,16 @@ function mostrarPersonas(){
     }
     document.getElementById('personas').innerHTML = texto;
 }
+function agregarPersona(){
+    const forma = document.forms['forma'];
+    const nombre = forma['nombre'];
+    const apellido = forma['apellido'];
+    if(nombre.value != '' && apellido.value != ''){
+    const persona = new Persona(nombre.value, apellido.value);
+    console.log(persona);
+    personas.push(persona);
+    mostrarPersonas();
+}else{
+    alert('No hay información que agregar')
+}
+}
