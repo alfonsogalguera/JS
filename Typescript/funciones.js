36ef20e6e83fdaef6beb85cca8c786dc37b5770a
Alfonso <galgueraalfonso@gmail.com>
2021-11-04 10:52:02 -0600

    a
    a

---
+++ Typescript/funciones.js
@@ -0,0 +1,14 @@
+"use strict";
+var saludo = "Hola mundo desde Typescript";
+saludo = "Hola";
+var numero = 10;
+var cualquiera = "cadena";
+cualquiera = 10;
+var pi = 3.1416;
+function saludar() {
+    console.log(saludo);
+    console.log(numero);
+    console.log(cualquiera);
+    console.log(pi);
+}
+saludar();
---
+++ Typescript/funciones.ts
@@ -0,0 +1,15 @@
+let saludo = "Hola mundo desde Typescript";
+saludo= "Hola";
+let numero:number = 10;
+
+let cualquiera:any = "cadena";
+cualquiera = 10;
+const pi = 3.1416;
+function saludar(){
+    console.log(saludo);
+    console.log(numero);
+    console.log(cualquiera);
+    console.log(pi);
+}
+
+saludar();
